b75f3f5b417d3f51e273e39f8b0117391a88bc41
laxect <laxect39@gmail.com>
2017-06-07 16:59:28 +0800

    add some comment for bilibili_spider

--- bilibili_spider.py
+++ bilibili_spider.py
@@ -10,6 +10,7 @@ class bilibili_spider:
         'aim in stand of which bangumi you want to watch'
         self.aim = []
         with open(sys.path[0]+'/bilibili_spider_config', 'r') as config:
+            # read aims from file
             for line in config:
                 if line:
                     self.aim.append(int(line))
@@ -21,25 +22,28 @@ class bilibili_spider:
         return url
 
     def _hash(self, content, aim):
+        'check if the content is new'
         import sys
         try:
             tmp = open(sys.path[0]+'/bilibili_spider_store', 'r+')
         except FileNotFoundError as err:
             print('Error : {err}')
             tmp = open(sys.path[0]+'/bilibili_spider_store', 'w+')
+        # read from file. and if the file is not exist, touch it
         try:
             data = json.loads(tmp.read())
         except json.decoder.JSONDecodeError:
             data = {}
+        # prevent if the file is new up and was empty
         tmp.close()
         if data.get(str(aim)) == content:
             res = False
         else:
             data[str(aim)] = content
             res = True
-        tmp = open(sys.path[0]+'/bilibili_spider_store', 'w+')
-        tmp.write(json.dumps(data))
-        tmp.close()
+        with open(sys.path[0]+'/bilibili_spider_store', 'w+') as tmp:
+            # write the date back
+            tmp.write(json.dumps(data))
         return res
 
     def handle(self, text, aim):

a812a08f6872a8b204b1913fa23b226d19416a91
laxect <laxect39@gmail.com>
2017-07-01 00:22:51 +0800

    config seek complete,

--- modules/database.py
+++ modules/database.py
@@ -4,7 +4,7 @@ from gevent.lock import Semaphore
 
 
 class database():
-    'laxect.database.3.2.0'
+    'laxect.database.3.4.0'
     # used to name as config_date
     _lock = Semaphore(1)
     # a global sqlite datbase lock.
@@ -45,14 +45,53 @@ class database():
             exec(f"self.sessions['{key}'] = {values}")
         return self.sessions
 
-    def new_session(self, key, values):
+    # in fact, this function was used at all.
+    def new_session(self, key, value):
+        """
+            add a new session to database.
+            Args :  key: Text, values: text
+            return : None
+        """
         database._lock.acquire()
         with sqlite3.connect(self.path) as db:
             cur = db.cursor()
-            sql = f"""insert into {self._id} values('{key}', "{values}")"""
+            sql = f"""insert into {self._id} values('{key}', "{value}")"""
             cur.execute(sql)
             db.commit()
         database._lock.release()
+
+    def session_update(self, key, new_value, table=None):
+        'standard session update func. also the back-end of config_update.'
+        if table is None:
+            table = self._id
+        database._lock.acquire()
+        with sqlite3.connect(self.path) as db:
+            cur = db.cursor()
+            sql = f'''update {table} set value={new_value} where key={key}'''
+            cur.execute(sql)
+            db.commit()
+        database._lock.release()
+
+    def config_update(self, key, value):
+        'the front of session_update'
+        self.sessions(key=key, value=value, table='config')
+
+    def session_seek(self, key, table=None):
+        'standard session seek func.'
+        if table is None:
+            table = self._id
+        sql = f'''select * from {table} where key="{key}"'''
+        database._lock.acquire()
+        with sqlite3.connect(self.path) as db:
+            cur = db.cursor()
+            cur.execute(sql)
+            res = cur.fetchall()
+            return str(res)
+        database._lock.release()
+
+    def config_seek(self, key):
+        'config seek func use session_seek'
+        return self.session_seek(key=key, table='config')
 
     # design for common usage.
     def __enter__(self):
--- modules/scales_bot.py
+++ modules/scales_bot.py
@@ -1,26 +1,45 @@
 import gevent
+from modules import database
 from telegram.ext import Updater, CommandHandler
 
 
 class scales_telegram_bot:
     'a telegram bot, has few function'
-    # laxect.scales_bot.1.0.0
+    # laxect.scales_bot.2.1.1
     def __init__(self, token, chat_id=None):
         self.id = 'laxect.telegram_bot'
         self.updater = Updater(token)
         self.chat_id = chat_id
+        self.db = database.database(self.id)
+
         self.updater.dispatcher.add_handler(
             CommandHandler('start', self.start)
         )
+        self.updater.dispatcher.add_handler(
+            CommandHandler('update', self.update)
+        )
+        self.updater.dispatcher.add_handler(
+            CommandHandler('seek', self.seek)
+        )
 
     def start(self, bot, update):
+        'do something with /start'
         update.message.reply_text('Hello World!')
         print(update.message.chat_id)
-        # design for test.
+
+    def seek(self, bot, update):
+        'seek config when /seek'
+        key = update.message['text'].split(' ')[1]
+        update.message.reply_text(self.db.config_seek(key))
+
+    def update(self, bot, update):
+        'update config when recvive /update'
+        _, key, value, *_ = update.message['text']
 
     def _run(self, queue):
-        item = queue.get()
-        self.updater.bot.send_message(self.chat_id, str(item))
+        while True:
+            item = queue.get()
+            self.updater.bot.send_message(self.chat_id, str(item))
 
     def run(self, queue):
         pool = [

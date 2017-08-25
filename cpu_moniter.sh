3f5fa316411313f160cd82f4d44766e84e108a74
laxect <laxect39@gmail.com>
2017-08-25 18:39:02 +0800

    debug fix

--- cpu_moniter.sh
+++ cpu_moniter.sh
@@ -4,7 +4,6 @@ foo=1
 while [ "${foo}" -le 200 ]
 do
     # echo $foo
-    python3.6 test_scale.py &
     ps aux | grep scale | grep -v "grep" | grep -v "cpu_moniter"
     sleep 0.1
     # foo=$(($foo+1))

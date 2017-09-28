2c9dad199256d4f836e86fb6a5995961c5895bb2
laxect <laxect.39@gmail.com>
2017-09-28 19:27:14 +0800

    db change to mode b

--- database.py
+++ database.py
@@ -1,16 +1,10 @@
 import sys
 import sqlite3
-from gevent.lock import Semaphore
 
 
 class database():
-    'laxect.database.3.6.4'
-    _lock = Semaphore(1)  # a global sqlite datbase lock.
-
     def __init__(self, sid='config'):
-        self.id = 'laxect.database'
-        self._id = sid.replace('.', '_')
-        # use for database table name
+        self._id = sid.replace('.', '_')  # use for database table name
         self.path = sys.path[0]+'/'+self.id+'.tmp'
         # be ware of that the sessions will not update automately.
         self.sessions = {}
@@ -20,7 +14,6 @@ class database():
 
     # check if the table is exist.
     def _init_check(self):
-        database._lock.acquire()
         with sqlite3.connect(self.path) as db:
             cur = db.cursor()
             try:
@@ -28,8 +21,7 @@ class database():
                 cur.fetchall()
             except sqlite3.OperationalError:
                 self._init_table(cur)
-            db.commit()
-        database._lock.release()
+                db.commit()
 
     # be warn of that this func will NOT acquire for lock.
     def _init_table(self, cur):
@@ -39,60 +31,53 @@ class database():
     # return a dict contains config.
     def loads(self):
         'load sessions from database.'
-        database._lock.acquire()
         with sqlite3.connect(self.path) as db:
             cur = db.cursor()
             cur.execute(f'select * from {self._id}')
             sessions = cur.fetchall()
-        database._lock.release()
         for key, values in sessions:
             exec(f'self.sessions["{key}"] = {values}')
         return self.sessions
 
-    # in fact, this function was not used at all.
-    def new_session(self, key, value):
+    def new_session(self, key, value, table=None):
         '''
             add a new session to database.
             Args :  key: Text, values: text
             return : None
         '''
-        database._lock.acquire()
+        table = table if table else self._id
+        sql = f'insert into {table} values("{key}", "{value}")'
         with sqlite3.connect(self.path) as db:
             cur = db.cursor()
-            sql = f'insert into {self._id} values("{key}", "{value}")'
             cur.execute(sql)
             db.commit()
-        database._lock.release()
         database.session_state = 'out_of_date'
 
     def session_update(self, key, value, table=None):
         'standard session update func. also the back-end of config_update.'
         if not table:
             table = self._id
-        database._lock.acquire()
+        sql = f'update {table} set value="{value}" where key="{key}"'
         with sqlite3.connect(self.path) as db:
             cur = db.cursor()
-            sql = f'update {table} set value="{value}" where key="{key}"'
             cur.execute(sql)
             db.commit()
-        database._lock.release()
 
     def session_seek(self, key, table=None):
         'standard session seek func.'
         if table is None:
             table = self._id
         sql = f'select * from {table} where key="{key}"'
-        database._lock.acquire()
         with sqlite3.connect(self.path) as db:
             cur = db.cursor()
             cur.execute(sql)
             res = cur.fetchall()
-        database._lock.release()
         return res
 
-    def check_up_to_date(self, cid, content):
+    # TODO need to de re wrote
+    def check_up_to_date(self, cid, content, table=None):
+        table = table if table else self._id
         check_result = False
-        database._lock.acquire()
         with sqlite3.connect(self.path) as db:
             cur = db.cursor()
             cur.execute(f'select value from "{self._id}" where key="{cid}"')
@@ -110,5 +95,4 @@ class database():
                 )
                 check_result = True
             db.commit()
-        database._lock.release()
         return check_result
